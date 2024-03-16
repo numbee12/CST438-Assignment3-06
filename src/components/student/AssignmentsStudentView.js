801ff53b31aaeb60a19c24b61e1563dcff34c583
Robin Hurtado <rohurtado@csumb.edu>
2024-03-15 18:18:19 -0700

    implemented and tested AssignmentsStudentView.js with dummy data

--- src/components/student/AssignmentsStudentView.js
+++ src/components/student/AssignmentsStudentView.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 
+
 // student views a list of assignments and assignment grades 
 // use the URL  /assignments?studentId= &year= &semester=
 // The REST api returns a list of SectionDTO objects
@@ -8,11 +9,40 @@ import React, {useState} from 'react';
 // display a table with columns  Course Id, Assignment Title, Assignment DueDate, Score
 
 const AssignmentsStudentView = (props) => {
-    
+
+    //dummy assignments and grades to test with
+    //Course Id, Assignment Title, Assignment DueDate, Score
+    const [assignments, setAssignment] = useState([
+        { courseId: 'cst363' , title: 'Introduction to Database', dueDate: '2024-02-01', score: 90},
+        { courseId: 'cst239' , title: 'Data Structures', dueDate: '2024-02-15', score: 95},
+        { courseId: 'cst311' , title: 'Networking', dueDate: '2022-01-30', score: 70},
+    ]);
+
+    const headers = ['Course ID', 'Assignment Title', 'Assigment Due Date', 'Score'];
+
      
     return(
         <> 
-            <h3>Not implemented</h3>   
+            <h3>Assignments</h3>
+
+            <table className="Center">
+                <thead>
+                    <tr>
+                        {headers.map((h,idx) => <th key={idx}>{h}</th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                {assignments.map((assignment) =>
+                    <tr key={assignment.courseId}>
+                        <td>{assignment.courseId}</td>
+                        <td>{assignment.title}</td>
+                        <td>{assignment.dueDate}</td>
+                        <td>{assignment.score}</td>
+                    </tr>
+                )}
+                </tbody>
+
+            </table>
         </>
     );
 }
